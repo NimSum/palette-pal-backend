6578f24638c686c7188ea6246383f46155d354ad
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-01 17:55:40 -0600

    Create get request method for all projects

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <!-- // lynne.rang@gmail.com -->
 
-git commit -m "Add dummy data for seeding
+git commit -m "Add seeding of dummy data and updates to dummy data structure
 
 
 Co-authored-by: Lynne <lynne.rang@gmail.com"
--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
 const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[environment];
-// const database = require('knex')(configuration);
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
 const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-
 app.set('port', process.env.PORT || 3001);
 
 app.listen(app.get('port'), () => {
   console.log(`App is running in port ${app.get('port')}`)
 });
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "Helloooo"});
-})
+app.get('/api/v1/projects', (req, res) => {
+  database('projects')
+  .select()
+  .then(projects => res.status(200).json(projects))
+  .catch(error => res.status(500).json({ error }))
+});
+
