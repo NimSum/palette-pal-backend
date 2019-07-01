c131d40add7978d10f6f510e4ca165746f6dda36
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-01 14:44:59 -0600

    Create bare-bones backend server
    Co-authored-by: Lynne <lynne.rang@gmail.com

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('./knexfile')[environment];
+// const database = require('knex')(configuration);
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.set('port', process.env.PORT || 3001);
+
+app.listen(app.get('port'), () => {
+  console.log(`App is running in port ${app.get('port')}`)
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Helloooo"});
+})
