a7acfed841fa5294c40436720ea5b0abff9c1281
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-01 18:23:45 -0600

    Create individual project get request path

--- server/server.js
+++ server/server.js
@@ -20,6 +20,22 @@ app.get('/api/v1/projects', (req, res) => {
   .catch(error => res.status(500).json({ error }))
 });
 
+app.get('/api/v1/projects/:id', (req, res) => {
+  database('projects')
+    .where({ id: req.params.id })
+    .then(project => {
+      if (!project.length) {
+        res.status(404).json({ 
+          error: 'No project matches the requested id' 
+        })
+      }
+      else {
+        res.status(200).json(...project)
+      };
+    })
+    .catch(error => res.status(500).json({ error }));
+})
+
 app.get('/api/v1/palettes', (req, res) => {
   database('palettes')
   .select()
@@ -33,5 +49,7 @@ app.get('/api/v1/palettes', (req, res) => {
   .then(palettes => res.status(200).json(palettes))
   .catch(error => res.status(500).json({ error }))
 });
+
+
 
 
