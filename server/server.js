f330568e201cae859150b2ad28f0f970456bf445
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-01 18:07:37 -0600

    Create get request method and path to all palettes, closes #9

--- server/server.js
+++ server/server.js
@@ -27,3 +27,11 @@ app.get('/api/v1/palettes', (req, res) => {
   .catch(error => res.status(500).json({ error }))
 });
 
+app.get('/api/v1/palettes', (req, res) => {
+  database('palettes')
+  .select()
+  .then(palettes => res.status(200).json(palettes))
+  .catch(error => res.status(500).json({ error }))
+});
+
+
