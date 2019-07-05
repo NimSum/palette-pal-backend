a1e6e00fc46248d70f130a69d9ef8d513a5c0de8
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-04 20:36:54 -0600

    Create schema for users

--- db/migrations/20190704203137_users.js
+++ db/migrations/20190704203137_users.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', table => {
+    table.increments('user_id').primary();
+    table.text('email').unique().notNullable();
+    table.text('password').notNullable();
+    table.boolean('is_active')
+      .notNullable()
+      .defaultTo(true)
+    table.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('users');
 };
