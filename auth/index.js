dc1e72f075a4bba4e34ebf4f0aae80bee5a90124
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-04 20:56:29 -0600

    Create method to check user existance in db

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 // Route paths are prepended with /auth
 router.get('/', (req, res) => {
   res.json({
@@ -8,27 +12,35 @@ router.get('/', (req, res) => {
   })
 })
 
-
-// User Requirements
-// Can login with valid email/password
-// Email is required - error for invalid emails
-// Password is required - error for incorrect
 function validateUser(user) {
   const validEmail = typeof user.email === 'string' &&
   user.email.trim() !== '';
   const validPassword = typeof user.password === 'string' &&
   user.password.trim() !== '' &&
-  user.password.trim() >= 6;
+  user.password.length >= 6;
   return validEmail && validPassword;                                          
 }
 
 router.post('/signup', (req, res) => {
+  const { email } = req.body;
+
   if (validateUser(req.body)) {
-    res.json({
-      message: 'GOOOD'
-    })
+    database('users')
+      .where({ email })
+      .first()
+      .then(user => {
+        if (!user) {
+          res.json({
+            user,
+            message: '✅'
+          })
+        } else {
+          res.status(400).json({ error: "Email in use" })
+        }
+      });
   } else {
     res.status(403).json({ error: "Invalid user" })
   }
 })
+
 module.exports = router;
