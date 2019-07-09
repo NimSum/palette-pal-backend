fd5207fcab343eccf60c34b666f4081dd19204b7
Lynne Rang <lynne.rang@gmail.com>
2019-07-08 19:30:58 -0600

    Add spread for id in post project response.

--- auth/index.js
+++ auth/index.js
@@ -6,91 +6,82 @@ require('dotenv').config();
 const db = require('../db');
 
 router.post('/signup', (req, res) => {
-  const { email, password, user_name } = req.body;
-  const validUserName = typeof user_name === 'string' &&
-  user_name.trim() !== '';
+	const { email, password, user_name } = req.body;
+	const validUserName = typeof user_name === 'string' && user_name.trim() !== '';
 
-  if (validateInputs(req.body) && validUserName) {
-    getUser(email)
-      .then(user => {
-        if (!user) {
-          bcrypt.hash(password, 10)
-            .then(hash => {
-              db('users')
-                .insert({ email, password: hash, user_name }, 'id')
-                .then(user_id => {
-                  addDefaultProject(user_id)
-                  res.status(201).json(...user_id)
-                })
-            })
-        } else {
-          res.status(400).json({ error: "Email in use" })
-        }
-      });
-  } else {
-    res.status(403).json({ error: "Invalid params, user_name, email, password required" })
-  }
-})
+	if (validateInputs(req.body) && validUserName) {
+		getUser(email).then(user => {
+			if (!user) {
+				bcrypt.hash(password, 10).then(hash => {
+					db('users').insert({ email, password: hash, user_name }, 'id').then(user_id => {
+						addDefaultProject(user_id);
+						res.status(201).json(...user_id);
+					});
+				});
+			} else {
+				res.status(400).json({ error: 'Email in use' });
+			}
+		});
+	} else {
+		res.status(403).json({ error: 'Invalid params, user_name, email, password required' });
+	}
+});
 
 router.post('/login', (req, res) => {
-  const { email, password } = req.body;
+  console.log(req.body)
+	const { email, password } = req.body;
 
-  if (validateInputs(req.body)) {
-    getUser(email)
-      .then(user => {
-        if (!user) {
-          res.status(403).json({ error: "Invalid login" })
-        } else {
-          bcrypt.compare(password, user.password)
-            .then(result => {
-              if (result) {
-                let options = {
-                  expiresIn: '7d'
-                }
-                jwt.sign({ user }, 'SECRETKEYGOESHERE', options, async (err, token) => {
-                  if (err) res.sendStatus(500);
-                  res.status(200).json({
-                    token,
-                    user_id: user.id,
-                    projects: await getUserData(user.id)
-                  })
-                })
-              } else res.status(403).json({ error: "Invalid login"})
-            })
-        }
-      })
-  } else {
-    res.status(403).json({ error: "Invalid login" })
-  }
-})
+	if (validateInputs(req.body)) {
+		getUser(email).then(user => {
+			if (!user) {
+				res.status(403).json({ error: 'Invalid login' });
+			} else {
+				bcrypt.compare(password, user.password).then(result => {
+					if (result) {
+						let options = {
+							expiresIn: '7d'
+						};
+						jwt.sign({ user }, 'SECRETKEYGOESHERE', options, async (err, token) => {
+							if (err) res.sendStatus(500);
+							res.status(200).json({
+								token,
+								user_id: user.id,
+								projects: await getUserData(user.id)
+							});
+						});
+					} else res.status(403).json({ error: 'Invalid login' });
+				});
+			}
+		});
+	} else {
+		res.status(403).json({ error: 'Invalid login' });
+	}
+});
 
-function getUserData(userId) {
-  return db.raw(`SELECT usr.id AS user_id, proj.project_name, proj.id AS project_id, pal.palette_name, pal.id AS palette_id, pal.color_1, pal.color_2, pal.color_3, pal.color_4, pal.color_5 FROM palettes AS pal RIGHT JOIN projects AS proj ON pal.project_id = proj.id LEFT JOIN users AS usr ON usr.id = proj.user_id WHERE usr.id IN (${res.auth.user.id}) ORDER BY proj.updated_at`)
-  .then(projects => {
-    return projects.rows;
-  })
+function getUserData(userId){
+	return db
+		.raw(
+			`SELECT usr.id AS user_id, proj.project_name, proj.id AS project_id, pal.palette_name, pal.id AS palette_id, pal.color_1, pal.color_2, pal.color_3, pal.color_4, pal.color_5 FROM palettes AS pal RIGHT JOIN projects AS proj ON pal.project_id = proj.id LEFT JOIN users AS usr ON usr.id = proj.user_id WHERE usr.id IN (${userId}) ORDER BY proj.updated_at`
+		)
+		.then(projects => {
+			return projects.rows;
+		});
 }
 
-async function addDefaultProject(userId) {
-  await db('projects')
-    .insert({ project_name: "Uncategorized", user_id: parseInt(userId) })
+async function addDefaultProject(userId){
+	await db('projects').insert({ project_name: 'Uncategorized', user_id: parseInt(userId) });
 }
 
-function validateInputs(user) {
-  const validEmail = typeof user.email === 'string' &&
-  user.email.trim() !== '';
+function validateInputs(user){
+	const validEmail = typeof user.email === 'string' && user.email.trim() !== '';
 
-  const validPassword = typeof user.password === 'string' &&
-  user.password.trim() !== '' &&
-  user.password.length >= 6;
+	const validPassword = typeof user.password === 'string' && user.password.trim() !== '' && user.password.length >= 6;
 
-  return validEmail && validPassword;
+	return validEmail && validPassword;
 }
 
-function getUser(email) {
-  return db('users')
-  .where({ email })
-  .first();
+function getUser(email){
+	return db('users').where({ email }).first();
 }
 
 module.exports = { router };
--- server.js
+++ server.js
@@ -8,191 +8,195 @@ const verifyToken = require('./middleware/verifyToken');
 const checkIfUserPalette = require('./middleware/checkUserPalettes');
 const checkIfUserProject = require('./middleware/checkUserProjects');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+  
 app.use(bodyParser.json());
 app.use('/auth', auth.router);
 app.use(cors());
 app.set('port', process.env.PORT || 3005);
 
 app.listen(app.get('port'), () => {
-  console.log(`App is running in port ${app.get('port')}`)
+	console.log(`App is running in port ${app.get('port')}`);
 });
 
 app.get('/api/v1/projects', verifyToken, (req, res) => {
-  const { palettes } = req.query;
-  if (palettes === 'included') {
-    db.raw(`SELECT usr.id AS user_id, proj.project_name, proj.id AS project_id, pal.palette_name, pal.id AS palette_id, pal.color_1, pal.color_2, pal.color_3, pal.color_4, pal.color_5 FROM palettes AS pal RIGHT JOIN projects AS proj ON pal.project_id = proj.id LEFT JOIN users AS usr ON usr.id = proj.user_id WHERE usr.id IN (${res.auth.user.id}) ORDER BY proj.updated_at`)
-    .then(projects => {
-      res.status(200).json(projects.rows)
-    })
-    .catch(error => res.status(500).json({ error }))
-  } else {
-    db('projects')
-      .select()
-      .then(projects => res.status(200).json(projects))
-      .catch(error => res.status(500).json({ error }))
-  }
+	const { palettes } = req.query;
+	if (palettes === 'included') {
+		db
+			.raw(
+				`SELECT usr.id AS user_id, proj.project_name, proj.id AS project_id, pal.palette_name, pal.id AS palette_id, pal.color_1, pal.color_2, pal.color_3, pal.color_4, pal.color_5 FROM palettes AS pal RIGHT JOIN projects AS proj ON pal.project_id = proj.id LEFT JOIN users AS usr ON usr.id = proj.user_id WHERE usr.id IN (${res
+					.auth.user.id}) ORDER BY proj.updated_at`
+			)
+			.then(projects => {
+				res.status(200).json(projects.rows);
+			})
+			.catch(error => res.status(500).json({ error }));
+	} else {
+		db('projects')
+			.select()
+			.then(projects => res.status(200).json(projects))
+			.catch(error => res.status(500).json({ error }));
+	}
 });
 
 app.get('/api/v1/projects/:id', verifyToken, (req, res) => {
-  db('projects')
-    .where({ id: req.params.id })
-    .then(project => {
-      if (!project.length || project[0].user_id !== res.auth.user.id) {
-        res.status(404).json({ 
-          error: 'Requested id does not correspond to any projects' 
-        })
-      }
-      else {
-        res.status(200).json(...project)
-      };
-    })
-    .catch(error => res.status(500).json({ error }));
-})
+	db('projects')
+		.where({ id: req.params.id })
+		.then(project => {
+			if (!project.length) {
+				res.status(404).json({
+					error: 'Requested id does not correspond to any projects'
+				});
+			} else if (project[0].user_id !== res.auth.user.id) {
+				res.status(403).json({
+					error: 'Requested id does not correspond to any projects for this user'
+				});
+			} else {
+				res.status(200).json(...project);
+			}
+		})
+		.catch(error => res.status(500).json({ error }));
+});
 
 app.get('/api/v1/palettes', (req, res) => {
-  const { project_id } = req.query;
+	const { project_id } = req.query;
 
-  if (project_id) {
-    db('palettes')
-      .where({ project_id })
-      .then(palettes => res.status(200).json(palettes))
-      .catch(error => res.status(500).json({ error }))
-  } else {
-    db('palettes')
-      .select()
-      .then(palettes => res.status(200).json(palettes))
-      .catch(error => res.status(500).json({ error }))
-  }
+	if (project_id) {
+		db('palettes')
+			.where({ project_id })
+			.then(palettes => res.status(200).json(palettes))
+			.catch(error => res.status(500).json({ error }));
+	} else {
+		db('palettes')
+			.select()
+			.then(palettes => res.status(200).json(palettes))
+			.catch(error => res.status(500).json({ error }));
+	}
 });
 
 app.get('/api/v1/palettes/:id', (req, res) => {
-  db('palettes')
-    .where({ id: req.params.id })
-    .then(palette => {
-      if (!palette.length) {
-        res.status(404).json({ 
-          error: 'Requested id does not correspond to any palettes' 
-        })
-      }
-      else {
-        res.status(200).json(...palette)
-      };
-    })
-    .catch(error => res.status(500).json({ error }));
-})
+	db('palettes')
+		.where({ id: req.params.id })
+		.then(palette => {
+			if (!palette.length) {
+				res.status(404).json({
+					error: 'Requested id does not correspond to any palettes'
+				});
+			} else {
+				res.status(200).json(...palette);
+			}
+		})
+		.catch(error => res.status(500).json({ error }));
+});
 
 app.post('/api/v1/projects', verifyToken, (req, res) => {
-  let { project_name } = req.body;
-  let user_id = parseInt(req.query.user_id);
-  
-  if (res.auth.user.id === user_id) {
-    if (!project_name)
-      return res.status(422).send({
-        error: 'Required parameter "project_name" is missing'
-    });
+	let { project_name } = req.body;
+	let user_id = parseInt(req.query.user_id);
 
-    db('projects').insert({ project_name, user_id }, 'id')
-    .then(projectId => res.status(201).json(projectId))
-    .catch(error => res.status(500).json({ error }))
-  } else {
-    res.sendStatus(403);
-  }
+	if (res.auth.user.id === user_id) {
+		if (!project_name)
+			return res.status(422).send({
+				error: 'Required parameter "project_name" is missing'
+			});
+
+		db('projects')
+			.insert({ project_name, user_id }, 'id')
+			.then(projectId => res.status(201).json(...projectId))
+			.catch(error => res.status(500).json({ error }));
+	} else {
+		res.sendStatus(403);
+	}
 });
 
 app.post('/api/v1/palettes', verifyToken, (req, res) => {
-  let newPalette = req.body;
-  let user_id = parseInt(req.query.user_id);
+	let newPalette = req.body;
+	let user_id = parseInt(req.query.user_id);
 
-  const parameters = [
-    'palette_name', 
-    'project_id',
-    'color_1', 
-    'color_2', 
-    'color_3', 
-    'color_4', 
-    'color_5', 
-  ];
+	const parameters = [ 'palette_name', 'project_id', 'color_1', 'color_2', 'color_3', 'color_4', 'color_5' ];
 
-  if (res.auth.user.id === user_id) {
-    for (let requiredParam of parameters) {
-      if (!newPalette[requiredParam] ) {
-        res.status(422).json({ error: 
-          `Expected parameters of: ${parameters.join(', ')}. Missing: ${ requiredParam }`})
-        return;
-      }
-    }
+	if (res.auth.user.id === user_id) {
+		for (let requiredParam of parameters) {
+			if (!newPalette[requiredParam]) {
+				res.status(422).json({
+					error: `Expected parameters of: ${parameters.join(', ')}. Missing: ${requiredParam}`
+				});
+				return;
+			}
+		}
 
-    db('palettes').insert(newPalette, 'id')
-    .then(paletteId => res.status(201).json(paletteId))
-    .catch(error => res.status(500).json({ error }))
-  } else {
-    res.sendStatus(403);
-  }
-})
+		db('palettes')
+			.insert(newPalette, 'id')
+			.then(paletteId => res.status(201).json(paletteId))
+			.catch(error => res.status(500).json({ error }));
+	} else {
+		res.sendStatus(403);
+	}
+});
 
 app.delete('/api/v1/projects/:id', verifyToken, checkIfUserProject, (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-    db('projects')
-    .where({ id })
-    .del()
-    .then((delCount) => {
-      if (!delCount) {
-        res.status(404).json({ 
-          error: 'Failed to Delete: Project does not exist' 
-        }) 
-      } else res.sendStatus(202);
-    })
-    .catch(error => res.status(500).json({ error }))
-})
+	db('projects')
+		.where({ id })
+		.del()
+		.then(delCount => {
+			if (!delCount) {
+				res.status(404).json({
+					error: 'Failed to Delete: Project does not exist'
+				});
+			} else res.sendStatus(202);
+		})
+		.catch(error => res.status(500).json({ error }));
+});
 
 app.delete('/api/v1/palettes/:id', verifyToken, checkIfUserPalette, (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  db('palettes')
-  .where({ id })
-  .del()
-  .then((delCount) => {
-    if (!delCount) {
-      res.status(404).json({ 
-        error: 'Failed to Delete: Palette does not exist' 
-      }) 
-    } else res.sendStatus(202);
-  })
-  .catch(error => res.status(500).json({ error }))
-})
+	db('palettes')
+		.where({ id })
+		.del()
+		.then(delCount => {
+			if (!delCount) {
+				res.status(404).json({
+					error: 'Failed to Delete: Palette does not exist'
+				});
+			} else res.sendStatus(202);
+		})
+		.catch(error => res.status(500).json({ error }));
+});
 
 app.put('/api/v1/projects/:id', verifyToken, checkIfUserProject, (req, res) => {
-  const { id } = req.params;
-  const { project_name } = req.body;
+	const { id } = req.params;
+	const { project_name } = req.body;
 
-  db('projects')
-  .where({ id })
-  .update({ project_name }, ['id'])
-  .then((id) => {
-    if (!id.length) {
-      res.status(404).json({ 
-        error: 'Failed to update: Project does not exist' 
-      });
-    } else res.sendStatus(202);
-  })
-  .catch(error => res.status(500).json({ error }))  
-})
+	db('projects')
+		.where({ id })
+		.update({ project_name }, [ 'id' ])
+		.then(id => {
+			if (!id.length) {
+				res.status(404).json({
+					error: 'Failed to update: Project does not exist'
+				});
+			} else res.sendStatus(202);
+		})
+		.catch(error => res.status(500).json({ error }));
+});
 
 app.put('/api/v1/palettes/:id', verifyToken, checkIfUserPalette, (req, res) => {
-  const { id } = req.params;
-  const updatedPalette = req.body;
+	const { id } = req.params;
+	const updatedPalette = req.body;
 
-    db('palettes')
-    .where({ id })
-    .update({ ...updatedPalette }, ['id'])
-    .then((id) => {
-      if (!id.length) {
-        res.status(404).json({ 
-          error: 'Failed to update: Palette does not exist' 
-        });
-      } else res.sendStatus(202);
-    })
-})
+	db('palettes').where({ id }).update({ ...updatedPalette }, [ 'id' ]).then(id => {
+		if (!id.length) {
+			res.status(404).json({
+				error: 'Failed to update: Palette does not exist'
+			});
+		} else res.sendStatus(202);
+	});
+});
 
 module.exports = app;
