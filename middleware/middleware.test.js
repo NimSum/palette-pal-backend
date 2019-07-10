99337ede560df38f5593d6aa45b0dd713d3839fd
Nimrod Garcia <nimm.garcia@gmail.com>
2019-07-09 22:53:21 -0600

    Test checkProjects middleware by mocking a full request

--- middleware/middleware.test.js
+++ middleware/middleware.test.js
@@ -4,8 +4,14 @@ const verifyToken = require('./verifyToken');
 const app = require('../server');
 const request = require('supertest');
 const mockData = require('../db/seeds/seeds-data/dummyData.json');
+const db = require('../db')
 
 describe('Middlewares', () => {
+
+  beforeEach(async () => {
+		await db.seed.run();
+  });
+
   describe('checkUserPalettes', () => {
     it('should intercept user before deletion if palette id does not belong to user id', async () => {
       const response = await request(app)
@@ -19,6 +25,25 @@ describe('Middlewares', () => {
       const response = await request(app)
         .delete('/api/v1/palettes/2')
         .set({ authorization: mockData.lynnardsToken })
+      expect(response.status).toBe(202);
+    })
+  })
+
+  describe('cherUserProjects', () => {
+    it('should intercept user before deletion if project id does not belong to user id', async () => {
+      const response = await request(app)
+        .delete('/api/v1/projects/1')
+        .set({ authorization: mockData.nimsumsToken })
+
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual({ error: "Not a user project or invalid id"});
+    })
+
+    it('should continue if project is owned by user', async () => {
+      const response = await request(app)
+        .delete('/api/v1/projects/2')
+        .set({ authorization: mockData.nimsumsToken })
+
       expect(response.status).toBe(202);
     })
   })
