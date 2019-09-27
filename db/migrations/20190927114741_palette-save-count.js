243d59768037e101dac56735bc25f63ddb886d30
Nimrod Garcia <nimm.garcia@gmail.com>
2019-09-27 12:17:41 -0500

    Create user save count column in palettes table

--- db/migrations/20190927114741_palette-save-count.js
+++ db/migrations/20190927114741_palette-save-count.js
@@ -1,9 +1,11 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.table('palettes', table => {
+    table.integer('user_save_count');
+  });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.table('palettes', table => {
+    table.dropColumn('user_save_count');
+  });
 };
-
